f4c2e99dd6f2d41fb7b1a78b5979ded7980e1432
bmanandhar <bijaya.manandhar@hotmail.com>
2018-04-18 21:32:19 -0700

    home work week1 day3

---
+++ js_hw_w1d3.rb
@@ -0,0 +1,33 @@
+
+# Wild Sum
+
+# Define a method, #wild_sum(n) that sums all the numbers less than n that are:
+#   - Divisible by 2 or divisible by 3
+#   - Not divisible by both 2 and 3
+
+# ex: wild_sum(16) ==> 2 + 3 + 4 + 8 + 9 + 10 + 14 + 15 ==> 65
+
+def wild_sum(n)
+  # your code here
+  wild_sum = 0
+
+  for i in 1 ... n
+
+    if i % 6 != 0 && i % 2 == 0
+      wild_sum += i
+
+    elsif i % 6 != 0 && i % 3 == 0
+      wild_sum += i
+    end
+
+
+  end
+  wild_sum
+
+end
+
+puts "------Wild Sum------"
+puts wild_sum(0) == 0
+puts wild_sum(2) == 0
+puts wild_sum(3) == 2
+puts wild_sum(7) == 9
