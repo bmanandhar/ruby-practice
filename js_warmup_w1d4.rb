0fb3252057d3eb18df2bbc77af5b80f97d0a7995
bmanandhar <bijaya.manandhar@hotmail.com>
2018-04-20 13:27:04 -0700

    this is warm up week1 day4

---
+++ js_warmup_w1d4.rb
@@ -0,0 +1,42 @@
+# Vowel Censor
+# ----------------------------------------
+# Write a method, #vowel_censor that takes in a string and replaces
+# any vowels in it with an "X". Do not modify the original string.
+
+def vowel_censor(string)
+
+  string.tr('aeiou', 'X')
+end
+
+puts "---------Vowel Censor----------"
+puts vowel_censor("let's order a pizza") == "lXt's XrdXr X pXzzX"
+puts vowel_censor("nitwit, blubber, oddment, tweak") == "nXtwXt, blXbbXr, XddmXnt, twXXk"
+puts vowel_censor("supercalifragilisticexpialidocious") == "sXpXrcXlXfrXgXlXstXcXxpXXlXdXcXXXs"
+
+# ----------------- Switch Roles!
+
+=begin
+# Largest Factor
+# ----------------------------------------
+# Write a method, #largest_factor,
+that accepts an integer as an argument and returns
+# the largest factor of that integer
+=end
+
+def largest_factor(n)
+
+  result = nil
+  (1 .. n/2).each do |d|
+    if n % d == 0
+      result = d
+    end
+  end
+
+  result
+end
+
+puts "---------Muni Routes----------"
+puts largest_factor(10) == 5
+puts largest_factor(143) == 13
+puts largest_factor(27) == 9
+puts largest_factor(17) == 1
